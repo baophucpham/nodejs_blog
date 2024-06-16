701a42d8a9b3d1cf6bb60d13b74734b579d380e9
Phạm Lê bảo phúc <phamlebaophuc@MacBook-Air-cua-Pham.local>
2024-06-16 19:51:53 +0700

    new commit

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = 3000;
 // Đây là router
 app.get("/trang-chu", (req, res) => res.send("Hello World!"));
 
+app.get("/tin-tuc",(req,res)=> res.send("Hello Tin tuc Page"))
+
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
 });
