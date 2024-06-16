920bd13d48fe77857b30f64cc26274dfd020e9d8
Phạm Lê bảo phúc <phamlebaophuc@MacBook-Air-cua-Pham.local>
2024-06-16 19:30:03 +0700

    new commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+// Đây là router
 app.get("/trang-chu", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => {
